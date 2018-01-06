e7559394fd07ffd010e67be34fa76fa7d83e60b8
Simon Legg <leggsimon@gmail.com>
2018-01-06 17:11:33 +0000

    update isATypeOfFunction to include variable assignments

--- test/lib/utils/astParse.spec.js
+++ test/lib/utils/astParse.spec.js
@@ -1,10 +1,18 @@
 const { filter } = require('../../../lib/utils/astParse');
 const acorn = require('acorn');
 
-const isATypeOfFunction = node =>
+const isATypeOfFunctionExpression = node =>
     node.type === 'FunctionDeclaration'
     || node.type === 'FunctionExpression'
     || node.type === 'ArrowFunctionExpression';
+
+const isAFunctionAssignedToVariable = node =>
+    node.type === 'VariableDeclarator'
+    && isATypeOfFunctionExpression(node.init);
+
+const isATypeOfFunction = (node, parentNode) =>
+    (isATypeOfFunctionExpression(node) && parentNode.type !== 'VariableDeclarator')
+    || (isAFunctionAssignedToVariable(node));
 
 test('can find a function', () => {
     const namedFunction = 'function named() {};';
@@ -30,10 +38,7 @@ test('returns the variable declaration node for assigned anonymous functions', (
         const func = function () {};
         const arrow = () => {};`;
     const ast = acorn.parse(assignedAnonFunction);
-    const result = filter(ast, node =>
-        node.type === 'VariableDeclarator'
-        && isATypeOfFunction(node.init)
-    );
+    const result = filter(ast, isATypeOfFunction);
     expect(result.length).toEqual(2);
     expect(
         result.every(({ type }) => type === 'VariableDeclarator')
@@ -48,12 +53,9 @@ test('returns function names or variable assignments', () => {
         function functionNamed1 () {}
         [].map(() => {})`;
     const ast = acorn.parse(functionVarsAndNames);
-    const result = filter(ast, (node, parentNode) =>
-        (isATypeOfFunction(node) && parentNode.type !== 'VariableDeclarator')
-        || (node.type === 'VariableDeclarator' && isATypeOfFunction(node.init))
-    );
+    const result = filter(ast, isATypeOfFunction);
     expect(result.length).toEqual(4);
-    const functions = result.map(node => (node.id && node.id.name) || undefined);
-    expect(functions).toEqual(['functionVar1', 'functionVar2', 'functionNamed1', undefined]);
+    const functionNames = result.map(node => (node.id && node.id.name) || undefined);
+    expect(functionNames).toEqual(['functionVar1', 'functionVar2', 'functionNamed1', undefined]);
 });
 
